6d016b92a638ee3b40bc3bdf6b7456c700a56e79
Xx-g <95361688+Xx-g@users.noreply.github.com>
2022-05-02 09:59:39 -0300

    Add files via upload

---
+++ service/ClienteService.java
@@ -0,0 +1,11 @@
+package br.senai.service;
+
+import br.senai.model.Cliente;
+
+import java.util.List;
+
+public interface ClienteService {
+
+    public List<Cliente> findAll();
+    public Cliente findByNome(String nome);
+}
---
+++ service/ClienteServiceImpl.java
@@ -0,0 +1,26 @@
+package br.senai.service;
+
+import br.senai.model.Cliente;
+import br.senai.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteServiceImpl implements ClienteService{
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @Override
+    public List<Cliente> findAll(){
+        return clienteRepository.findAll(Sort.by("nome"));
+    }
+
+    @Override
+    public Cliente findByNome(String nome) {
+        return clienteRepository.findByNome(nome);
+    }
+}
